542abe020292f1ef9b7e1ac8d3dbfb1d2bd4bd79
Danter28 <danter28@gmail.com>
2019-09-02 18:31:45 -0600

    estructura base Pabellones

--- Pabellones.cpp
+++ Pabellones.cpp
@@ -1,1 +1,15 @@
 #include "Pabellones.h"
+
+Pabellones::Pabellones() {}
+Pabellones::Pabellones(char sector, char genero):genero(genero),sector(sector) {
+	Camas* cama=NULL;
+	for (int contador = 0; contador <= camas.size(); contador++) {
+		cama = cama->Camas(sector, contador);
+		camas[contador] = cama;
+	}
+	delete cama;
+}
+char Pabellones::getSectorPabellon() {return sector;}
+char Pabellones::getGeneroPabellon() {return genero;}
+void Pabellones::setGeneroPabellon(char&) {}
+void Pabellones::setSectorPabellon(char&) {}
--- Pabellones.h
+++ Pabellones.h
@@ -2,10 +2,22 @@
 #define _PABELLONES_H
 #include"Paciente.h"
 #include"Camas.h"
+class Camas;
 class Pabellones{
+protected:
+string sector;
+char genero;
+Camas * camas[20];
+
 private:
 
 public:
+	Pabellones();
+	Pabellones(char,char);
+	char getSectorPabellon();
+	char getGeneroPabellon();
+	void setGeneroPabellon(char&);
+	void setSectorPabellon(char&);
 
 };
 #endif
