0659626ae6745a9a87671890852428707f6c1b7c
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-09-04 11:01:54 -0600

    implementacion de doctor

--- Doctor.cpp
+++ Doctor.cpp
@@ -1,1 +1,21 @@
 #include "Doctor.h"
+
+Doctor::Doctor() {
+
+}
+
+Doctor::Doctor(string, Especialidad *) {
+
+}
+
+void Doctor::setNombre(string& nombre) {
+    this->nombre(nombre);
+}
+
+string Doctor::getNombre() {
+    return nombre;
+}
+
+void Doctor::setEspecialidad(Especialidad * especialidad) {
+    this->especialidad(especialidad);
+}
