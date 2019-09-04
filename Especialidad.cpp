67bddae908add7962fa3012672d526bdc921e2b7
Dylan Molina <damolina27@hotmail.com>
2019-09-04 13:10:02 -0600

    Metodos set, get  y constructores

--- Especialidad.cpp
+++ Especialidad.cpp
@@ -1,1 +1,13 @@
 #include "Especialidad.h"
+Especialidad::Especialidad() {};
+Especialidad::Especialidad(string especial) {
+	especialidades = especial;
+}
+
+void Especialidad:: setEspecialidades(string espe) {
+	especialidades = espe;
+}
+
+string Especialidad:: getEspecialidades() {
+	return especialidades;
+}
--- Especialidad.h
+++ Especialidad.h
@@ -1,10 +1,14 @@
 #ifndef _ESPECIALIDAD_H
 #define _ESPECIALIDAD_H
+using std::string;
 class Especialidad{
 private:
-
+	string especialidades;
 public:
 
-
+	Especialidad();
+	Especialidad(string);
+	void setEspecialidades();
+	string getEspecialidades();
 };
 #endif
