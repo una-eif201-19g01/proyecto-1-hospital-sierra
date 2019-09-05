91e4566ab8115a865a5935e91a537ebcbe642b95
Joseph Araya Moore <49999761+JosephArayaM@users.noreply.github.com>
2019-09-05 15:23:43 -0600

    Paso opcion 3 a 4 y  4 a 5 y agrego ingresar doctores

--- Main.cpp
+++ Main.cpp
@@ -21,8 +21,8 @@ int main() {
 }
 
 void menu(Pabellon pab[10],Doctor doc[20],Especialidad esp[10],Paciente p1,Cama *cam){
-	int respuesta,op1,op2,op3,estado,doc1,cont;
-	string esp1,esp2,doc2,cedula,nombre,apellido,direccion,patologia,tipoCirugia,prioridad,fechaCirugia,genero;
+	int respuesta,op1,op2,op3,op4,estado,doc1,cont;
+	string esp1,esp2,doc2,doc3,cedula,nombre,apellido,direccion,patologia,tipoCirugia,prioridad,fechaCirugia,genero;
 	bool rs1;
 	do {
 		cout<<"------------------------------------------------------------------------"<<endl;
@@ -30,15 +30,16 @@ void menu(Pabellon pab[10],Doctor doc[20],Especialidad esp[10],Paciente p1,Cama 
 		cout<<"-----------------------------------------------------------------------"<<endl;
 		cout<<"   * 1. Internar un paciente "<<endl;
 		cout<<"   * 2. Ingresar especialidades "<<endl;
-		cout<<"   * 3. Eliminar doctores "<<endl;
-		cout<<"   * 4. Salir del sistema"<<endl;
+		cout<<"   * 3. Ingresar doctores "<<endl;
+		cout<<"   * 4. Eliminar doctores "<<endl;
+		cout<<"   * 5. Salir del sistema"<<endl;
 		cout<<"-----------------------------------------------------------------------"<<endl;
 		cout<<"   * Ingrese su seleccion: "<<endl;
 		cout<<"-----------------------------------------------------------------------"<<endl;
 		cin>>respuesta;
 		cout<<endl;
 		
-		switch (respuesta) {
+		switch (respuesta){
 		case 1:
 			cout<<"------------------------------------------------------------------------"<<endl;
 			cout<<"   * En cual especialidad va a ingresar el paciente?:  "<<endl;
@@ -99,6 +100,9 @@ void menu(Pabellon pab[10],Doctor doc[20],Especialidad esp[10],Paciente p1,Cama 
 			}
 			break;
 		case 2:
+			cout<<"------------------------------------------------------------------------"<<endl;
+			cout<<"   * Bienvenido al sistema para ingresar Especialidades *  "<<endl;
+			cout<<"------------------------------------------------------------------------"<<endl;
 			for(int es1=0;es1<10;es1++){
 			cout<<"------------------------------------------------------------------------"<<endl;
 			cout<<"   * Ingrese el nombre de la especialidad:  "<<endl;
@@ -110,11 +114,38 @@ void menu(Pabellon pab[10],Doctor doc[20],Especialidad esp[10],Paciente p1,Cama 
 			break;
 		case 3:
 			cout<<"------------------------------------------------------------------------"<<endl;
+			cout<<"   * Bienvenido al sistema para ingresar doctores *  "<<endl;
+			cout<<"------------------------------------------------------------------------"<<endl;
+			for(int do2=1;do2<11;do2++){
+				cout<<"------------------------------------------------------------------------"<<endl;
+				cout<<"   * Ingrese el nombre del doctor:  "<<endl;
+				cout<<"------------------------------------------------------------------------"<<endl;
+				cin>>doc3;
+				cout<<"------------------------------------------------------------------------"<<endl;
+				cout<<"   * Las especialidades disponibles son:  "<<endl;
+				cout<<"------------------------------------------------------------------------"<<endl;
+				cont=1;
+				for(int es1=1;es1<11;es1++){
+					cout<<cont2<<"   * "<<esp[es1]<<endl;
+					cout<<endl;
+					cont++;
+				}
+				cout<<"------------------------------------------------------------------------"<<endl;
+				cout<<"   * Ingrese el numero de la especialidad:  "<<endl;
+				cout<<"------------------------------------------------------------------------"<<endl;
+				cin>>op4;
+				doc[do2]->setNombre(doc3);
+				doc[do2]->setEspecialidad(*esp[op4]);
+			}
+			
+			break;
+		case 4:
+			cout<<"------------------------------------------------------------------------"<<endl;
 			cout<<"   * Los doctores actuales son:  "<<endl;
 			cout<<"------------------------------------------------------------------------"<<endl;
 			cont=1;
-			for(int doc2=1;doc2<11;doc2++){
-				cout<<cont<<"   * "<<doc[doc2]<<endl;
+			for(int do1=1;do1<11;do1++){
+				cout<<cont<<"   * "<<doc[do1]<<endl;
 				cout<<endl;
 				cont++;
 			}
@@ -141,7 +172,7 @@ void menu(Pabellon pab[10],Doctor doc[20],Especialidad esp[10],Paciente p1,Cama 
 			cin>>op3;
 			doc[doc1]->setNombre(doc2);
 			doc[doc1]->setEspecialidad(*esp[op3]);
-			}
+			
 			break;
 		default:
 			cout<<"-----------------------------------------------------------------------------"<<endl;
@@ -150,5 +181,5 @@ void menu(Pabellon pab[10],Doctor doc[20],Especialidad esp[10],Paciente p1,Cama 
 			cout<<"-----------------------------------------------------------------------------"<<endl;
 			cout<<endl;
 		}
-	} while (respuesta != 4);
+	} while (respuesta != 5);
 }
