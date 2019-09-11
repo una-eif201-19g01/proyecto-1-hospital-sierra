96d6128f0e32cf9b8120db48cde9ef8dfc9e0e01
Dylan Molina <damolina27@hotmail.com>
2019-09-11 17:19:15 -0600

    metodo get especialidad de clase doctor

--- Doctor.cpp
+++ Doctor.cpp
@@ -14,9 +14,12 @@ string Doctor::getNombre() {
 void Doctor::setEspecialidad(Especialidad * especialidad) {
     this->especialidad(especialidad);
 }
+Especialidad* Doctor::getEspecialidad() {
+	return especialidad->getEspecialidades;
+}
 
 std::string Doctor::imprimeDoctor() {
 	std::string reporte;
-	reporte += "Nombre del Doctor: " + getNombre() + "\nEspecialidad: " + std::to_string(getEspecialidad());
+	reporte += "Nombre del Doctor: " + getNombre() + "\nEspecialidad: " + std::to_string(getEspecialidades());
 	return reporte;
 }
