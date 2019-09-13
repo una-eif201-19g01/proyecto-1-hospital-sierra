6f369cd3ad70297951e2733e3867b109846212b8
Danter28 <38162330+Danter28@users.noreply.github.com>
2019-09-13 15:50:14 -0600

    Update Paciente.cpp

--- Paciente.cpp
+++ Paciente.cpp
@@ -1,22 +1,26 @@
 #include "Paciente.h"
 
-Paciente::Paciente() {}
+Paciente::Paciente() :cedula(""), nombre(""), apellido(""),
+direccion(""), patologia(""), tipoCirugia(""), estado(1), prioridad(""), fechaCirugia(""),genero("") {
+	Cama* cama = NULL;
+	Doctor* doctor = NULL;
+}
 Paciente::Paciente(string& cedula, string& genero, string& nombre, string& apellido, string& direccion, string& patologia,
-	string& tipoCirugia, int estado, string& prioridad, string& fecha, Cama* cama, Doctor* doctor) :cedula(cedula), nombre(nombre), apellido(apellido),
-	direccion(direccion), patologia(patologia), tipoCirugia(tipoCirugia), estado(estado), prioridad(prioridad), cama(cama), fechaCirugia(fecha), doctor(doctor),
+	string& tipoCirugia, int estado, string& prioridad, string& fecha, Doctor* doctor) :cedula(cedula), nombre(nombre), apellido(apellido),
+	direccion(direccion), patologia(patologia), tipoCirugia(tipoCirugia), estado(estado), prioridad(prioridad), fechaCirugia(fecha), doctor(doctor),
 	genero(genero) {}
-void Paciente::setCedula(string& cedula) { this->cedula = cedula; }
-void Paciente::setGenero(string& genero) { this->genero = genero; }
-void Paciente::setNombre(string& nombre) { this->nombre = nombre; }
-void Paciente::setApellido(string& apellido) { this->apellido = apellido; }
-void Paciente::setDireccion(string& direccion) { this->direccion = direccion; }
-void Paciente::setPatologia(string& patologia) { this->patologia = patologia; }
-void Paciente::setTipoCirugia(string& tipo) { tipoCirugia = tipo; }
+void Paciente::setCedula(string& cedula) { this->cedula=cedula; }
+void Paciente::setGenero(string& genero) { this->genero=genero; }
+void Paciente::setNombre(string& nombre) { this->nombre=nombre; }
+void Paciente::setApellido(string& apellido) { this->apellido=apellido; }
+void Paciente::setDireccion(string& direccion) { this->direccion=direccion; }
+void Paciente::setPatologia(string& patologia) { this->patologia=patologia; }
+void Paciente::setTipoCirugia(string& tipo) { tipoCirugia=tipo; }
 void Paciente::setEstado() { estado = (estado == 0) ? 1 : 0; }
-void Paciente::setPrioridad(string& prioridad) { this->prioridad = prioridad; }
-void Paciente::setFechaCirugia(string& fecha) { this->fechaCirugia = fecha; }
-void Paciente::setCama(Cama* cama) { this->cama = cama; }
-void Paciente::setDoctor(Doctor* doctor) { this->doctor = doctor; }
+void Paciente::setPrioridad(string& prioridad) { this->prioridad=prioridad; }
+void Paciente::setFechaCirugia(string& fecha) { this->fechaCirugia=fecha; }
+void Paciente::setCama(Cama* cama) { this-> cama=cama; }
+void Paciente::setDoctor(Doctor* doctor) { this->doctor=doctor; }
 string Paciente::getGenero() { return genero; }
 string Paciente::getCedula() { return cedula; }
 string Paciente::getNombre() { return nombre; }
@@ -36,6 +40,6 @@ std::string Paciente::imprimePaciente() {
 		"\nGenero:" + getGenero() + "\nDireccion: " + getDireccion() + "\nPatologia: " + getPatologia() +
 		"\nTipo de Cirugia: " + getTipoCirugia() + "\nEstado: " + std::to_string(getEstado()) +
 		"\nPrioridad: " + getPrioridad() + "\nFechaCirugia: " + getFechaCirugia() + "\nNumero Cama: " +
-		getCama() + "\nDoctor: " + getDoctor();
+		 getCama() + "\nDoctor: " + getDoctor();
 	return reporte;
 }
