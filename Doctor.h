8d750a682e29b0f6b815a7b73e35ab268cc4d5b9
Dylan Molina <damolina27@hotmail.com>
2019-09-13 19:57:06 -0600

    Documentacion de Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -13,15 +13,36 @@ private:
 	int tamannoDoctor;
 public:
 	Doctor();
+	//Constructor sin parametros
+
 	Doctor(string, Especialidad*, int);
+	//Constructor con parametros
+	//Recibe un string, un puntero a la clase especialidad y un entero
+
         ~Doctor();
+		//Destructor
+
 	void setNombre(string&);
+	//Modifica el nombre del doctor y recibe un string por referencia
+
 	string getNombre();
+	//obtiene el nombre del doctor
+
 	void setEspecialidad(Especialidad*);
+	//Modifica la especialidad recibiendo por puntero a Especialidad
+
 	string getEspecialidad();
+	//Obtiene la especialidad del doctor
+
 	string imprimeDoctor();
+	//Da formato para mostrar a los doctores y sus especialidades
+
 	string imprimePacientes();
+	//Da formato para mostrar a los pacientes
+
 	void ingresarPaciente(Paciente* paciente);
+	//Permite ingresar a un paciente
+	//Recibe por parametro a un puntero de Paciente
 
 };
 #endif
