dcc33099fc73e6d20568bffe8dda8e252433f6df
Dylan Molina <damolina27@hotmail.com>
2019-09-13 19:53:08 -0600

    Documentacion de la clase pabellon.h

--- Pabellon.h
+++ Pabellon.h
@@ -14,15 +14,34 @@ private:
 
 public:
 	Pabellon();
+	//Constructor sin parametros
+
 	Pabellon(char, char, int);
+	//Constructor con Parametros
         ~Pabellon();
+		//Destructor
 	char getSectorPabellon();
+	//obtiene el sector del pabellon
+
 	char getGeneroPabellon();
+	//obtiene el genero del pabellon
+
 	void setGeneroPabellon(char&);
+	//Modifica el Genero segun el pabellon
+	//Recibe un char por parametro de referencia 
+
 	void setSectorPabellon(char&);
+	// Modifica el sector del pabellon
+	//recibe un char por parametro por parametro de referencia 
+
 	void ingresarPaciente(Paciente*);
+	//Permite ingresar un paciente accediendo por puntero a la clase paciente
+
 	void liberarCama(int);
+	//Recibe un parametro entero en la posicion donde se desea eliminar la cama
+
 	std::string imprimePabellon();
+	//Da formato para mostrar los pabellones del arreglo
 
 };
 #endif
